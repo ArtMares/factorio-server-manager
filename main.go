d3c88558b41692784c1f2b1cf582fe293024940b
Dmitriy Dergachev <dergachevdv90@gmail.com>
2019-01-25 16:35:46 +0300

    Review

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "bitbucket.org/aisgteam/ai-app"
+
+func main() {
+    aiapp.Vendor = "influ.su"
+    aiapp.Name = "Factorio Manager"
+    aiapp.Ver = aiapp.Version{0,0,1}
+
+
+}
